4b9ad6aacfee7e5d6edecce4dd8615e29dd960c9
luukm <luuk.machielse@gmail.com>
2025-06-27 17:53:40 +0200

    Centered the O-piece in its matrix

--- src/main.rs
+++ src/main.rs
@@ -181,9 +181,9 @@ impl TetrominoKind {
                 [false, false, false, false],
             ],
             TetrominoKind::O => [
-                [false, true, true, false],
-                [false, true, true, false],
                 [false, false, false, false],
+                [false, true, true, false],
+                [false, true, true, false],
                 [false, false, false, false],
             ],
             TetrominoKind::T => [
